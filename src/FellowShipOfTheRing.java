b69712a2f762f120ca02bd2aac2648e9e112b945
Admin <koszegfalvi.gabor@gmail.com>
2023-06-15 19:22:28 +0200

    rdy

--- src/FellowShipOfTheRing.java
+++ src/FellowShipOfTheRing.java
@@ -1,6 +1,37 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class FellowShipOfTheRing {
     public static void main(String[] args) {
-       // TODO
+
+        Set<String> dwarves = new HashSet<>(List.of("Gimli"));
+
+        Set<String> elves = new HashSet<>(List.of("Legolas"));
+
+        Set<String> humans = new HashSet<>(List.of("Aragorn", "Boromir"));
+
+        Set<String> hobbits = new HashSet<>(List.of("Frodo", "Sam", "Merry", "Pippin"));
+
+        Set<String> fellowship = new HashSet<>();
+        //Set<Set<String>> fellowship = new HashSet<>();
+        fellowship.addAll(dwarves);
+        fellowship.addAll(elves);
+        fellowship.addAll(humans);
+        fellowship.addAll(hobbits);
+
+        System.out.println(fellowship);
+        fellowship.add("Gandalf");
+        fellowship.remove("Gandalf");
+        fellowship.add("Gandalf The White");
+        fellowship.removeAll(hobbits);
+        System.out.println(fellowship.containsAll(hobbits));
+        fellowship.remove("Boromir");
+        System.out.println(fellowship);
+
     }
+
 }
+
+
